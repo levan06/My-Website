e3aad0b56b5c6f14b1b1cd9f7ad786f9d1fc4c00
Abdellah <abdoachafik@gmail.com>
2026-07-01 16:19:38 +0100

    Add Logout button

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,7 @@
 <?php
 
+/* Verify if a sesion exist */
+/*   and compare its data   */
 session_start();
 
 if ( !isset( $_SESSION[ 'user_id' ] ) )
@@ -8,8 +10,34 @@ if ( !isset( $_SESSION[ 'user_id' ] ) )
     exit();
 }
 
-echo "Welcome " . htmlspecialchars( $_SESSION[ "name" ] ) . 
-     " your mail is : " . htmlspecialchars( $_SESSION[ "email" ] );
+/* Loging Out */
+if( isset( $_POST[ "logout" ] ) )
+{
+    header( 'Location : index.php' );
+    exit();
+}
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Dashboard</title>
+        <link type="text/css" rel="stylesheet" href="dashboard.css">
+    </head>
+
+    <body>
+
+        <p>
+            <?= "Welcome " . htmlspecialchars( $_SESSION[ "name" ] ) . 
+                " your mail is : " . htmlspecialchars( $_SESSION[ "email" ] ); 
+            ?>
+        </p>
+
+        <form method="post" action="">
+            <button name="logout" id="btnLogout" type="submit">Log Out</button>
+        </form>
+
+    </body>
+</html>
