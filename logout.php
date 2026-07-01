e2fd02c42fd3534754ee12cc43781bd6d68de5dd
Abdellah <abdoachafik@gmail.com>
2026-07-01 16:47:33 +0100

    Update logout, removing cookie from the browser too

--- logout.php
+++ logout.php
@@ -4,7 +4,23 @@ session_start();
 
 $_SESSION = [];
 
+if ( ini_get( "session.use_cookies" ) )
+{
+    $params = session_get_cookie_params();
+
+    /* Delete the brpwers's session cookie too */
+    setcookie(
+        session_name(),
+        '',
+        time() - 3600,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
 session_destroy();
 
-header( 'Location: index.php' );
+header("Location: index.php");
 exit();
