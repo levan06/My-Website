6a22ddd8bb51dc7039dad33247073b907ed639b5
Abdellah ACHAFIK <abdoachafik@gmail.com>
2026-09-17 17:55:44 +0200

    Add default values for database connection
    Set default values for host and port if not provided.

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 
-$host = getenv("PGHOST");
-$port = getenv("PGPORT");
+$host = getenv("PGHOST") ?: '127.0.0.1'; // Uses '127.0.0.1' if getenv returns false or empty;
+$port = getenv("PGPORT") ?: '5432';
 $db   = getenv("PGDATABASE");
 $user = getenv("PGUSER");
 $pass = getenv("PGPASSWORD");
